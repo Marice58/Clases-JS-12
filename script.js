9081cfa1f6195b7851f2cdebe26bd4e0714b9aec
Marice58 <marydelira1@gmail.com>
2022-04-27 13:21:40 -0300

    Clase JS

---
+++ script.js
@@ -0,0 +1,62 @@
+//Aca meto un popup una linea
+
+alert ( "Hola Mundo" )
+/*
+series
+comentarios
+*/
+
+let alumna = "Lucia";
+let profesor = "Juan Carlos" 
+let edad = 20;
+
+alumna = "Eugenia"
+
+console.log( "La alumna "+ alumna  + "tiene " + edad + " y tiene de profe a :" + profesor);
+
+let Alumno
+let _apellido
+let $edad
+let nombreApellido = "emiliano spinoso"
+nombre_Apellido = "ana maria"
+NombreApellido= "ana clara"
+
+///Variables que estan mal
+/*
+let 1nombre
+let nombre-_apellido
+let break, super, this
+*/
+
+////Tipos de datos ////
+
+//Strings
+let str1 = "String con comillas dobles"
+let str2 = `String con comillas simples`
+let str3 = `String con Backticks`
+
+console.log( str1 + str2 )
+
+console.log("numerito" + "y ademas " + "num2")
+
+let numero1 = parseInt(prompt("mete el numero 1"));
+let numero2 = parseInt(prompt("mete el segundo numero"))
+
+
+let resta = numero1-numero2
+let suma = numero1+numero2
+let multi = numero1*numero2
+let division = numero1/numero2
+
+console.log("Calculadora")
+console.log("la suma es :" + suma)
+console.log("la resta es :" + resta)
+console.log("la multiplicacion es :" + multi)
+console.log("la division es :" + division)
+
+//booleanos
+let verdadera = true
+let falsa = false
+
+let respuesta = 3>4
+console.log(respuesta)
